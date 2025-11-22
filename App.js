e5f817578d54e62f29c94a2d4e51c37a235ce273
Akshay Raut <rautakshay78@gmail.com>
2025-11-23 00:10:46 +0530

    parcel integrated for build

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>h1 tag</h1>
-        <h2>h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>h1 tag</h1>
-        <h2>h2 tag</h2>
-    </div>
-</div> */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h1", {}, "h1 bbvbvb tag"),
     React.createElement("h2", {}, "h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
